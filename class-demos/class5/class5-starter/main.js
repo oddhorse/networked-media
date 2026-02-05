9aa167af0de91738b559e482838095a7fb893108
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-02-05 16:57:37 -0500

    whateverrrrrrr

--- class-demos/class5/class5-starter/main.js
+++ class-demos/class5/class5-starter/main.js
@@ -6,5 +6,15 @@ window.onload = () => {
 	console.log("webpage is fully loaded")
 
 	document.getElementById("main").innerHTML = "PEWWWW"
-	document.getElementById("main").style.backgroundColor = 'red'
+	// document.getElementById("main").style.backgroundColor = 'red'
+
+	let main = document.getElementById("main")
+	// main.style.color = 'lightgreen'
+
+	main.classList.add("blue")
+
+	let container = document.getElementById("container")
+	let item = document.createElement("p")
+	item.textContent = "dynamically created item"
+	container.appendChild(item)
 }

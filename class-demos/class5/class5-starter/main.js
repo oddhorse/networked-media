da0be0352b9009ac5a96d4e55fe598af5d0e621f
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-02-05 16:43:18 -0500

    idk man im just in class committing shit to commit it man whatever man

---
+++ class-demos/class5/class5-starter/main.js
@@ -0,0 +1,10 @@
+// main.js
+alert("hello!")
+
+
+window.onload = () => {
+	console.log("webpage is fully loaded")
+
+	document.getElementById("main").innerHTML = "PEWWWW"
+	document.getElementById("main").style.backgroundColor = 'red'
+}

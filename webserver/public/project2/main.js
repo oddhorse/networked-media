733124c536200beb16ddb40e634609cf2871de0f
oddhorse <15389659+oddhorse@users.noreply.github.com>
2026-02-19 15:06:43 -0500

    added congregation and sound effects and stuff like that

--- webserver/public/project2/main.js
+++ webserver/public/project2/main.js
@@ -3,29 +3,46 @@
 	main.js */
 
 window.onload = () => {
-	// DEMO/TEST BUTTONS
+	//=====DEMO/TEST ELEMENTS=====//
+
+	//const time = document.getElementById("time")
 	const demoRingBtn = document.getElementById("demo-ring-btn")
 	const demoRingTwiceBtn = document.getElementById("demo-ring-twice-btn")
 	const demoRingSixBtn = document.getElementById("demo-ring-six-btn")
+	const demoCongregateBtn = document.getElementById("demo-congregate-btn")
+	const demoDisperseBtn = document.getElementById("demo-disperse-btn")
 
-	const bell = new Audio('bell.mp3')
+	//=====ELEMENTS AND STATE=====//
+
+	const bell = new Audio("bell.mp3")
+	const crowdTalk = new Audio("crowd-talking.mp3")
+	const shh = new Audio("shh.mp3")
+	const slam = new Audio("slam.mp3")
 	const church = document.getElementById("church")
-	const time = document.getElementById("time")
-	let ringsRemaining = 0
+	const congregation = document.getElementById("congregation")
+	const pageTitle = document.getElementById("page-title")
 
-	// RECURSIVE BELL HANDLING
+	let ringsRemaining = 0
+	let lastHourRung
+	let congregating = false
+
+	//=====RECURSIVE BELL HANDLING=====//
 	// not biggest recursive fan in the world but simplest way i know to handle the timed nature
 
 	/**
 	 * starts church bell ringing number of times.
-	 * @param {number} count number of times it plays
+	 * @param {number} count - number of times it plays
 	 */
 	function ringChurchBell(count) {
 		ringsRemaining = count - 1 // decrements right before bell plays so we can catch it at 0
-		console.log(`ringing ${count} times!`)
+		console.log(`tolling ${count} times!`)
 		bell.play()
 	}
 
+	/**
+	 * handles recursive re-ringing of bell, handling remaining rings count.
+	 * not meant to be run(g) except on finished-ringing callback!
+	 */
 	function ringChurchBellAgain() {
 		if (ringsRemaining === 0) {
 			console.log("ringing done!")
@@ -36,34 +53,147 @@ window.onload = () => {
 		bell.play()
 	}
 
-	bell.addEventListener("ended", (event) => {
-		console.log("bell done")
+	bell.addEventListener("playing", () => {
+		pageTitle.innerText = "the bell tolls"
+	})
 
+	bell.addEventListener("ended", () => {
+		console.log("bell done")
+		pageTitle.innerText = "church"
 		ringChurchBellAgain()
 	})
 
+	// DEMO BUTTONS
 	demoRingBtn.addEventListener("click", () => {
 		ringChurchBell(1)
 	})
-
 	demoRingTwiceBtn.addEventListener("click", () => {
 		ringChurchBell(2)
 	})
-
 	demoRingSixBtn.addEventListener("click", () => {
 		ringChurchBell(6)
 	})
 
+	//=====CONGREGATION=====//
+	/**
+	 * gather congregation
+	 */
+	function goToChurch() {
+		if (congregating) {
+			console.log("already congregating")
+			return
+		}
+		congregating = true
+		crowdTalk.play()
+		congregation.animate(
+			[
+				{ left: "100%", top: "50%", scale: "80%" },
+				{ left: "45%", top: "calc(60% - 30px)", scale: "40%" },
+			],
+			{
+				duration: 2500,
+				iterations: 1,
+				delay: 0,
+				easing: "linear",
+			}
+		)
+		church.animate(
+			[
+				{ scale: "100%" },
+				{ scale: "103%" },
+				{ scale: "100%" },
+			],
+			{
+				duration: 100,
+				iterations: 1,
+				delay: 2400,
+				easing: "steps(2)",
+			}
+		)
+		setTimeout(() => {
+			slam.play()
+		}, 2400)
+	}
 
+	/**
+	 * disperse congregation
+	 */
+	function leaveChurch() {
+		if (!congregating) {
+			console.log("nobody's in there")
+			return
+		}
+		congregating = false
+		crowdTalk.play()
+		slam.play()
+		congregation.animate(
+			[
+				{ left: "45%", top: "calc(60% - 30px)", scale: "-40% 40%" }, // note neg x val to flip image
+				{ left: "100%", top: "50%", scale: "-80% 80%" },
+			],
+			{
+				duration: 2500,
+				iterations: 1,
+				delay: 100,
+				easing: "linear",
+			}
+		)
+		church.animate(
+			[
+				{ scale: "100%" },
+				{ scale: "103%" },
+				{ scale: "100%" },
+			],
+			{
+				duration: 100,
+				iterations: 1,
+				delay: 0,
+				easing: "steps(2)",
+			}
+		)
+	}
+
+	// DEMO BUTTONS
+	demoCongregateBtn.addEventListener("click", () => {
+		goToChurch()
+	})
+	demoDisperseBtn.addEventListener("click", () => {
+		leaveChurch()
+	})
+
+
+	//=====TIMER EVENTS!!!!!!=====//
 
 	setInterval(() => {
-		let min = new Date().getMinutes()
-		let hour = new Date().getHours()
-		time.innerText = `${hour}:${min}`
+		let time = new Date()
+		let hr = time.getHours()
+		let hr12 = (hr + 24) % 12 || 12 // https://stackoverflow.com/questions/10556879/changing-the-1-24-hour-to-1-12-hour-for-the-gethours-method
+		let min = time.getMinutes()
+		let sec = time.getSeconds()
+		let day = time.getDay() // sunday === 0
+
+		// ring bell every hour
+		if (min === 0 && sec == 0) ringChurchBell(hr12)
+
+		// on sundays
+		if (day === 0) {
+			// early congregation 
+			if (hr === 9 && min === 15 && sec === 0) goToChurch()
+			if (hr === 10 && min === 30 && sec === 0) leaveChurch()
+
+			// late congregation
+			if (hr === 10 && min === 45 && sec === 0) goToChurch()
+			if (hr === 12 && min === 0 && sec === 0) leaveChurch()
+		}
 	}, 1000)
 
 	church.addEventListener("click", () => {
-		ringChurchBell(1)
+		//ringChurchBell(1)
+		goToChurch()
 	})
+
+	//=====UTILITY=====//
+
+	//function ()
 }
 
